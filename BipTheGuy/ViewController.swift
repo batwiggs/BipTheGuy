90063caab0993a65792d72ee8ad77ebe4934cb38
Brennan Twiggs <btwiggs@zagmail.gonzaga.edu>
2022-02-15 12:23:35 -0800

    Completed 3.6

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var audioPlayer: AVAudioPlayer!
+    var imagePickerController = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagePickerController.delegate = self
     }
     
     func playSound(name: String) {
@@ -44,11 +45,11 @@ class ViewController: UIViewController {
     @IBAction func photoOrCameraPressed(_ sender: UIButton) {
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
-            print("You clicked Photo Library")
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { [self] (_) in
+            self.accessPhotoLibrary()
         }
         let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
-            print("You clicked Camera")
+            self.accessCamera()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
@@ -79,3 +80,30 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = originalImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func accessPhotoLibrary() {
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    func accessCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerController.sourceType = .camera
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            showAlert(title: "Camera Not Available", message: "There is no Camera available on this device.")
+        }
+    }
+}
